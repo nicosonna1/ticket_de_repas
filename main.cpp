c52a27aaec274920d48bb067c571564641a3e278
Calum-QuinnHEIG <114938794+Calum-QuinnHEIG@users.noreply.github.com>
2022-10-06 17:59:26 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,76 +1,45 @@
-// Fichier     main.cpp
-// Auteur      Quinn Calum - Nicolas Sonnard
-// Date        29.09.2022
-//
-// But         Calculer le prix d'un repas
-//
-// Remarque
-//
-// Modifs      Date / Auteur / Raison
-//
-// Compilateur Bundled MinGW - Version w64 9.0
-
 #include <iostream>
 #include <iomanip>
 #include <limits>
 
-// test
-
 using namespace std;
+
 int main() {
-    // Déclaration des constantes
+    //Définition des variables
+    const float PRIXENTREE  = 4.50,
+                PRIXPLAT    = 12.50,
+                PRIXBOISSON = 3.00,
+                PRIXDESSERT = 6.00,
+                PRIXCAFE    = 2.80;
 
-    // taille des colones
-    const int W =25;
+    int     nbEntree,
+            nbPlat,
+            nbBoissons,
+            nbDessert,
+            nbCafe;
+
+    float   coutEntree,
+            coutPlat,
+            coutBoisson,
+            coutDessert,
+            coutCafe,
+            coutTotal;
 
 
-    // Accueil de l'utilisateur
-
-    cout    << "bonjour, ce programme ..." << endl
-            << "voici les conditions" << endl
-            << "================================" << endl
-            << left << setw(W) << " - Prise en charge " << ":  " << "5.00" << endl
-            << left << setw(W) << " - supp par bagage " << ":  " << "2.50" << endl
-            << left << setw(W) << " - tarif/minute " << ":  " << "1.20" << endl
-            << left << setw(W) <<endl << endl;
-    // Demande des infos à l'utilisateur
-    cout    << "votre commande" << endl
-            << "================================" << endl;
-
-    cout    << left << setw(W) << "  - nbr de bagage" << ": ";
-
-    // Demandes du nombre de bagage
-    cin >> nbBagages;
+    cout << "Combien de boissons?" <<;
+    cin >> nbBoissons >>;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    cout    << left << setw(W) << " - distance [km]" << ": ";
-    cin >> distanceTotale;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if (nbBoissons >= 0 && nbBoissons <= 10) {
+        cout << "Merci de votre "
+    }
+    else {
+        cout << "";
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        return 0;
+    }
 
-    cout    << left << setw(W) << " - vitesse [km/h]" << ": ";
-    cin >> vitesseMoyenne;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    // Calcul de la surtaxe des bagages
-    float surtaxeBagage = (float) nbBagages * SURTAXE_PAR_BAGAGE;
 
-    // Calcul du temps de la course ( Distance / Vitesse moyenne * 60)
-    float tempsCourse = distanceTotale / vitesseMoyenne * 60;
-
-    // Calcul du prix de la course (Tarif par minute * Temps de la course)
-    float prixCourse = tempsCourse * TARIF_PAR_MINUTE;
-
-    // Impression du ticket final à l'utilisateur
-    cout    << fixed << setprecision(2);
-    cout    << "votre ticket" << endl
-            << "==================================" << endl
-            << left << setw(W) <<  " - prise en charge" << ": " << TAXE_BASE << endl
-            << setw(W) <<  " - supp bagage" << ": " << surtaxeBagage << endl
-            << setw(W) <<  " - prix de la course" << ": "  << prixCourse << endl
-            << setw(W) <<  "  TOTAL" << ": " << prixCourse + surtaxeBagage + TAXE_BASE << endl << endl
-            << setw(W) <<  "presser ENTER pour quitter";
-
-    // Cin et attente du enter de l'utilisateur pour quitter le programme
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
     return 0;
 }
