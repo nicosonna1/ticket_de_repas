d6f578158f3854498cd078f867b30d0ba7f23c81
Nicolas Sonnard <114679645+nicosonna1@users.noreply.github.com>
2022-10-10 17:44:37 +0200

    final code
    final

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 // Fichier     main.cpp
 // Auteur      Quinn Calum - Nicolas Sonnard
-// Date        29.09.2022
+// Date        6.10.2022
 //
 // But         Calculer le prix d'un repas
 //
@@ -18,12 +18,10 @@
 using namespace std;
 
 // define
-
-#define CLEAR_BUFFER cin.ignore(numeric_limits<streamsize>::max(), '\n')
-#define EXIT_PROGRAMME 0
-#define TICKET_OK  1
-#define TICKET_KO  0
-#define RETOUR_LIGNE cout << endl
+#define CLEAR_BUFFER    cin.ignore(numeric_limits<streamsize>::max(), '\n')
+// exit selon la constante EXIT_SUCCESS
+#define EXIT_PROGRAMME  0
+#define RETOUR_LIGNE    cout << endl
 
 
 int main() {
@@ -35,7 +33,7 @@ int main() {
     // largeur des collones du tableau 
     const int LARG_COL = 25;
     //largeur du ":"
-    const int LARG_COL_P = 3;
+    const int LARG_COL_P = 7;
 
 
     //Définition des variables
@@ -45,27 +43,31 @@ int main() {
                 PRIXDESSERT = 6.00,
                 PRIXCAFE = 2.80;
 
-    int     nbEntree,
-            nbPlat,
-            nbBoissons,
-            nbDessert,
-            nbCafe;
+    int         nbEntree,
+                nbPlat,
+                nbBoissons,
+                nbDessert,
+                nbCafe;
 
+    float       prixTotal;
 
-    bool ticketStatut = TICKET_KO;
+    /*  ici on doit initialiser car on part du principe que le ticket est non valide tant que
+        l'utilisateur à pas saisi de valeurs */
+
+    bool ticketStatut = false;
 
 
     //Salutations et sortie du menu
     cout << "Bonjour je suis votre serveur, voici le menu: " << endl
 
-         << "===============================" << endl;
+         << "===================================" << endl;
 
-    cout << fixed << setprecision(2);
-    cout << left << setw(LARG_COL) << "Entree"         << setw(LARG_COL_P) << ":" << PRIXENTREE  << endl
-                 << setw(LARG_COL) << "Plat principal" << setw(LARG_COL_P) << ":" << PRIXPLAT    << endl
-                 << setw(LARG_COL) << "Boisson"        << setw(LARG_COL_P) << ":" << PRIXBOISSON << endl
-                 << setw(LARG_COL) << "Dessert"        << setw(LARG_COL_P) << ":" << PRIXDESSERT << endl
-                 << setw(LARG_COL) << "Cafe"           << setw(LARG_COL_P) << ":" << PRIXCAFE    << endl;
+    cout    << fixed   << setprecision(2);
+    cout    << left    << setw(LARG_COL) << "Entree"           << ":" << right   << setw(LARG_COL_P) << PRIXENTREE  << endl
+            << left    << setw(LARG_COL) << "Plat principal"   << ":" << right   << setw(LARG_COL_P) << PRIXPLAT    << endl
+            << left    << setw(LARG_COL) << "Boisson"          << ":" << right   << setw(LARG_COL_P) << PRIXBOISSON << endl
+            << left    << setw(LARG_COL) << "Dessert"          << ":" << right   << setw(LARG_COL_P) << PRIXDESSERT << endl
+            << left    << setw(LARG_COL) << "Cafe"             << ":" << right   << setw(LARG_COL_P) << PRIXCAFE    << endl;
 
 
     //Définition des restrictions de saisie
@@ -108,42 +110,45 @@ int main() {
                     CLEAR_BUFFER;
 
                     if (nbCafe >= QTE_MIN && nbCafe <= QTE_MAX) {
-                        ticketStatut = TICKET_OK;
+                        ticketStatut = true;
                     }
                 }
             }
         }
     }
 
-    //Résumé de la commande
+    //Résumé de la commande seulement si ticket valide
     if (ticketStatut) {
         cout << endl << "Merci pour votre commande, en voici un resume: " << endl;
 
-        cout << "=============================" << endl;
+        cout << "===================================" << endl;
 
         cout << fixed << setprecision(2);
-        cout << left << setw(LARG_COL) << "Nb d'entrees"   << setw(LARG_COL_P) << ":" << nbEntree   << endl
-                     << setw(LARG_COL) << "Nb de plats"    << setw(LARG_COL_P) << ":" << nbPlat     << endl
-                     << setw(LARG_COL) << "Nb de boissons" << setw(LARG_COL_P) << ":" << nbBoissons << endl
-                     << setw(LARG_COL) << "Nb de desserts" << setw(LARG_COL_P) << ":" << nbDessert  << endl
-                     << setw(LARG_COL) << "Nb de cafes"    << setw(LARG_COL_P) << ":" << nbCafe     << endl;
+        cout    << setw(LARG_COL)           << "Nb d'entrees"   << ":" << right  << setw(LARG_COL_P)   << nbEntree    << endl
+                << left << setw(LARG_COL)   << "Nb de plats"    << ":" << right  << setw(LARG_COL_P)   << nbPlat      << endl
+                << left << setw(LARG_COL)   << "Nb de boissons" << ":" << right  << setw(LARG_COL_P)   << nbBoissons  << endl
+                << left << setw(LARG_COL)   << "Nb de desserts" << ":" << right  << setw(LARG_COL_P)   <<  nbDessert  << endl
+                << left << setw(LARG_COL)   << "Nb de cafes"    << ":" << right  << setw(LARG_COL_P)   <<  nbCafe     << endl;
         RETOUR_LIGNE;
         RETOUR_LIGNE;
 
         //Ticket final avec couts
+        prixTotal = (float)nbEntree*PRIXENTREE+(float)nbPlat*PRIXPLAT+(float)nbBoissons*PRIXBOISSON+(float)nbDessert*PRIXDESSERT+(float)nbCafe*PRIXCAFE;
         cout << left << setw(LARG_COL) << "Votre ticket " << endl
 
-             << "===============================" << endl;
+             << "===================================" << endl;
 
         cout << fixed << setprecision(2);
-        cout << nbEntree   << setw(LARG_COL) << (nbEntree<=1    ?    " Entree"  :" Entrees")   << setw(LARG_COL_P) << ":" << (float)nbEntree*PRIXENTREE    << endl
-             << nbPlat     << setw(LARG_COL) << (nbPlat<=1      ?    " Plat"    :" Plats")     << setw(LARG_COL_P) << ":" << (float)nbPlat*PRIXPLAT        << endl
-             << nbBoissons << setw(LARG_COL) << (nbBoissons<=1  ?    " Boisson" :" Boissons")  << setw(LARG_COL_P) << ":" << (float)nbBoissons*PRIXBOISSON << endl
-             << nbDessert  << setw(LARG_COL) << (nbDessert<=1   ?    " Dessert" :" Desserts")  << setw(LARG_COL_P) << ":" << (float)nbDessert*PRIXDESSERT  << endl
-             << nbCafe     << setw(LARG_COL) << (nbCafe<=1      ?    " Cafe"    :" Cafes")     << setw(LARG_COL_P) << ":" << (float)nbCafe*PRIXCAFE        << endl
-                           << setw(LARG_COL) << "   Total"      << setw(LARG_COL_P) << " :" << /*setw(LARG_COL_P) <<*/ (float)nbEntree*PRIXENTREE+(float)nbPlat*PRIXPLAT+(float)nbBoissons*PRIXBOISSON+(float)nbDessert*PRIXDESSERT+(float)nbCafe*PRIXCAFE  << endl;
+        cout << nbEntree   << setw(LARG_COL) << (nbEntree<=1    ?    " Entree"  :" Entrees")                << ":"  << right    << setw(LARG_COL_P) << (float)nbEntree*PRIXENTREE    << endl
+                << left << nbPlat     << setw(LARG_COL) << (nbPlat<=1      ?    " Plat"    :" Plats")       << ":"  << right    << setw(LARG_COL_P) << (float)nbPlat*PRIXPLAT        << endl
+                << left << nbBoissons << setw(LARG_COL) << (nbBoissons<=1  ?    " Boisson" :" Boissons")    << ":"  << right    << setw(LARG_COL_P) << (float)nbBoissons*PRIXBOISSON << endl
+                << left << nbDessert  << setw(LARG_COL) << (nbDessert<=1   ?    " Dessert" :" Desserts")    << ":"  << right    << setw(LARG_COL_P) << (float)nbDessert*PRIXDESSERT  << endl
+                << left << nbCafe     << setw(LARG_COL) << (nbCafe<=1      ?    " Cafe"    :" Cafes")       << ":"  << right    << setw(LARG_COL_P) << (float)nbCafe*PRIXCAFE        << endl
+                << left               << setw(LARG_COL) << "   Total"   << " :" << right << setw(LARG_COL_P) << prixTotal                                                            << endl;
         cout << "Merci beaucoup de votre visite!" << endl;
-    } else {
+    }
+    // message d'erreur saisie invalide
+    else {
         cout << "Vous avez saisi une quantite impossible, merci de vous en aller." << endl;
     }
 
